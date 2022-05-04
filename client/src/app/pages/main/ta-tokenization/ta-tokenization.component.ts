052f4fc673a1f3e5ac6aec478c0d2ecc56a5dc6c
Nagesh Deva <meetnag@yahoo.com>
2022-05-04 18:09:53 +0530

    few changes6

--- client/src/app/pages/main/ta-tokenization/ta-tokenization.component.ts
+++ client/src/app/pages/main/ta-tokenization/ta-tokenization.component.ts
@@ -280,11 +280,12 @@ export class TaTokenizationComponent implements OnInit {
         data.agreement2 +
         "   " +
         data.agreement3 +
-        "  ";
+        "  " + 
+        data.agreement4;
       const resp = await this.connectService.createToken(
         data.tokens, //numberOfToken,
         agr,
-        data.description
+        data.data
       );
       // const resp = true;
       // const tokenId = 5241;
